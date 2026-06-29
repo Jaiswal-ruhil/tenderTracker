bea6c01c8181a340fbd59969035132c18ec5eb51
Ruhil Jaiswal <ruhiljaiswal@gmail.com>
2026-06-29 12:08:11 +0530

    scaper improve ments

--- scraper.py
+++ scraper.py
@@ -1,6 +1,18 @@
 import re
 import time
 
+# Static import hooks for PyInstaller package scanning
+try:
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.chrome.service import Service
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+    from webdriver_manager.chrome import ChromeDriverManager
+except ImportError:
+    pass
+
 _selenium_ok = False
 def _try_import_selenium():
     global _selenium_ok
